66479ef60e9d6fe118b891d504299f19f68c17a0
Tom Butterworth <bangnoise@gmail.com>
2015-02-11 11:29:30 +0000

    Full-screen on all displays

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,17 @@
 #include "ofMain.h"
 #include "ofApp.h"
+#include "ofAppGLFWWindow.h"
 
 //========================================================================
 int main( ){
+    /*
+    Cause OF to go fullscreen over all displays
+    http://forum.openframeworks.cc/t/fullscreen-dual-screen/693/26
+    */
+    ofAppGLFWWindow window;
+    window.setMultiDisplayFullscreen(true);
+	ofSetupOpenGL(&window, 1024,768,OF_WINDOW);
 
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
-    
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
 	ofRunApp(new ofApp());
 
 }
